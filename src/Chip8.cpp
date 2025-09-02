4006fedaef876f3b4379743b74815071a3f5ae49
ashab-k <ashabkhan2000@gmail.com>
2025-09-02 14:27:40 +0530

    feat: implements multiple instructions

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -60,3 +60,80 @@ const unsigned int FONTSET_START_ADDRESS = 0x50;
 void Chip8::OP_00E0(){
     memset(video, 0 , sizeof(video));
 }
+
+void Chip8::OP_00EE(){
+    --sp;
+    pc = stack[sp];
+}
+
+void Chip8::OP_1nnn(){
+    uint16_t address = opcode & 0x0FFFu; // masking
+    pc = address;
+}
+
+void Chip8::OP_2nnn(){
+    uint16_t address = opcode & 0x0FFFu;
+    stack[sp] = pc;
+    ++sp;
+    pc = address;
+}
+
+void Chip8::OP_3xkk(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    if (registers[Vx] == byte){
+        pc += 2;
+    }
+}
+
+void Chip8::OP_4xkk(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    if (registers[Vx] != byte){
+        pc += 2;
+    }
+}
+
+void Chip8::OP_5xy0(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    if (registers[Vx] == registers[Vy]){
+        pc += 2;
+    }
+}
+
+void Chip8::OP_6xkk(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = (opcode & 0x00FFu);
+    registers[Vx] = byte;
+}
+
+void Chip8::OP_7xkk(){
+    uint8_t  Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    registers[Vx]+= byte;
+}
+
+void Chip8::OP_8xy0(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    registers[Vx] = registers[Vy];
+}
+
+void Chip8::OP_8xy1(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    registers[Vx] |= registers[Vy];
+}
+
+void Chip8::OP_8xy2(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    registers[Vx] &= registers[Vy];
+}
+
+void Chip8::OP_8xy3(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    registers[Vx] ^= registers[Vy];
+}
